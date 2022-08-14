6d25933d2dcef5ce08aa603069275e74e67232fb
Arunkumar Ramasamy <arunkumar.ramasamy@intellectdesign.com>
2022-08-14 14:46:07 +0530

    #update procfile

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-const url = 'mongodb://localhost/crudDb'
+const url = 'mongodb://127.0.0.1:27017/crudDb'
 
 const app = express()
 
-mongoose.connect(url, {useNewUrlParser:true});
+mongoose.connect(url, {useNewUrlParser:true}).then((res) => {
+    console.log('mongoose connected ... %j', res);
+}).catch((reason) => {
+    console.log('reason %j', reason);
+});
+
+
 
 const connection = mongoose.connection;
 connection.on('open', (con) => {
